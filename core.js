fd60a2fda39a38a246f694936d1db6f2cb844fad
Marekk2k <marekpl183@gmail.com>
2024-05-26 15:54:46 +0200

    Update core.js
    Hi. Please check your email regarding adding a translation

--- core.js
+++ core.js
@@ -18,6 +18,7 @@ var available_langs = {
     "it_it": { "name": "Italiano", "file": "it_it.json"},
     "jp_jp": { "name": "日本語", "file": "jp_jp.json"},
     "pt_br": { "name": "Português do Brasil", "file": "pt_br.json"},
+    "pl_pl": { "name": "Polski", "file": "pl_pl.json"},
     "ru_ru": { "name": "Русский", "file": "ru_ru.json"},
     "tr_tr": { "name": "Türkçe", "file": "tr_tr.json"},
     "zh_cn": { "name": "中文", "file": "zh_cn.json"},
